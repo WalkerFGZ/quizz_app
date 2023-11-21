c4b9ba76e11ba5c85f8f31986258d4ffbf7b41c1
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2023-11-20 20:47:52 -0500

    ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/quizz-app',
+  base: 'https://walkerfgz.github.io/quizz_app/',
 })
